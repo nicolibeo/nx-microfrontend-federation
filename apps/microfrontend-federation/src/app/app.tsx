5d199c889f3b7a8c6bc419a2644d468ddbfc1466
nicolibeo <nicolas.labbe@libeo.io>
2022-01-17 09:44:14 +0100

    initial commit

---
+++ apps/microfrontend-federation/src/app/app.tsx
@@ -0,0 +1,14 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <style jsx>{`
+        /** your style here **/
+      `}</style>
+      <NxWelcome title="microfrontend-federation" />
+    </>
+  );
+}
+
+export default App;
